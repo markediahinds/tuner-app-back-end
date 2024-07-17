2f3feaeade9838c716232b70599e45b62f9cf8a6
Markedia Hinds <mhinds@pursuit.org>
2024-07-17 09:50:29 -0400

    Validations Complete

--- controllers/songsController.js
+++ controllers/songsController.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const songs = express.Router()
 const { getAllSongs, getSong, createSong } = require(`../queries/songs`)
+const { checkSong, checkBoolean, checkArtist, checkAlbum, checkTime } = require('../validations/checkSongs')
+
 
 songs.get('/', async (req, res) => {
     const allSongs = await getAllSongs()
     if(allSongs[0]) {
         res.status(200).json(allSongs)
     } else {
-        res.status(500).json({ error: "Internal Service Error" })
+        res.status(500).json({ error: 'Internal Service Error' })
     }
 })
 
@@ -17,13 +19,22 @@ songs.get('/:id', async (req, res) => {
     if(singleSong) {
         res.status(200).json(singleSong)
     } else {
-    res.status(404).json({ error: `Song Not Found`})
+    res.status(404).json({ error: 'Song Not Found' })
     }
 })
 
-songs.post('/', async (req, res) => {
+songs.post('/', checkSong, checkBoolean, checkArtist, checkAlbum, checkTime, async (req, res) => {
     const newSong = await createSong(req.body)
     res.json(newSong)
+})
+
+songs.delete('/:id', async (req, res) => {
+    const deletedSong = await deleteColor(id)
+    if(deletedSong.id) {
+        res.status(200).json({ message: 'Successfully deleted Song' })
+    } else {
+        res.status(404).json({ error: 'Song Not Found' })
+    }
 })
 
 
---
+++ validations/checkSongs.js
@@ -0,0 +1,42 @@
+const checkSong = (req, res, next) => {
+    if (req.body.song) {
+        return next()
+    } else {
+        res.status(400).json({ error: `What Song you wanna Sing?` })
+    }
+}
+
+const checkBoolean = (req, res, next) => {
+    if(req.body.is_alive) {
+        return next()
+    } else {
+        res.status(400).json({ error: `is_alive must be True or False` })
+    }
+}
+
+const checkArtist = (req, res, next) => {
+    if(req.body.artist) {
+        next()
+    } else {
+        res.status(400).json({ error: `We're going to need an Artist` })
+    }
+}
+
+const checkAlbum = (req, res, next) => {
+    if(req.body.album) {
+        next()
+    } else {
+        res.status(400).json({ error: `What Album?`})
+    }
+}
+
+const checkTime = (req, res, next) => {
+    if(req.body.time) {
+        next()
+    } else {
+        res.status(400).json({ error: `Provide the length of the song as well`})
+    }
+}
+
+
+module.exports = { checkSong, checkBoolean, checkArtis, checkAlbum, checkTime }
